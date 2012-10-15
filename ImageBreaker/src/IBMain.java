c37e0b798679395fc07bdef0b5b450003ee8f249
Allen <Allink28@gmail.com>
2012-10-15 13:35:06 -0400

    Code reorganized to be much cleaner. Sub images now save to the location the selected image came from.

--- ImageBreaker/src/IBMain.java
+++ ImageBreaker/src/IBMain.java
@@ -1,40 +1,16 @@
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.JFileChooser;
-import javax.swing.SwingUtilities;
-
 /**
- * Breaks images into several different 1024x1024 images (as well as any remainder).
  * 
  * @author Allen
  *
  */
-public class IBMain implements Runnable{
+public class IBMain {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		 SwingUtilities.invokeLater(new IBMain()); //Complying with Swing's threading policy.
-		 //http://docs.oracle.com/javase/6/docs/api/javax/swing/package-summary.html#threading
+		ImageSlicer imageCutter = new ImageSlicer(1024,1024);
+		//System.out.println("Done. You have used MTIB");
 	}
 
-	@Override
-	public void run() {
-		JFileChooser fc = new JFileChooser();
-		ImageSlicer imgSlicer;
-		if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
-			BufferedImage img = null;
-			try {
-			    img = ImageIO.read(fc.getSelectedFile());
-			} catch (IOException e) {
-				System.exit(1);
-			}
-			imgSlicer = new ImageSlicer(img, fc.getName(fc.getSelectedFile()));			
-			imgSlicer.slice(1024, 1024);			
-		}
-		
-	}
 }
--- ImageBreaker/src/ImageSlicer.java
+++ ImageBreaker/src/ImageSlicer.java
@@ -1,7 +1,9 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 /**
@@ -10,20 +12,29 @@ import javax.swing.JOptionPane;
  *
  */
 public class ImageSlicer {
-	BufferedImage img;
-	String[] fileInfo;
+		
+	private BufferedImage img;
+	private String parentFile;
+	private String[] fileInfo;	
+	private int width;
+	private int height;
 	
-	public ImageSlicer(BufferedImage i, String filename){
-		img = i;
-		fileInfo = filename.split("\\.");
-	}
+	/**
+	 * Constructor
+	 * @param width Horizontal size of sub-images
+	 * @param height Vertical size of sub-images
+	 */
+	public ImageSlicer(int width, int height){
+		this.width = width;
+		this.height = height;
+		getImage();
+		slice();
+	}	
 
 	/**
-	 * Breaks the given image up and saves the sub images to the user's desktop.
-	 * @param width Width of Subimages
-	 * @param height Height of Subimages
+	 * Breaks img up and saves the sub images to the same folder img came from.
 	 */
-	public void slice(int width, int height){		
+	public void slice(){		
 		int x = img.getWidth()/width;
 		int y = img.getHeight()/height;
 		
@@ -50,27 +61,51 @@ public class ImageSlicer {
 				if (j+1 == y && img.getHeight()%height != 0){
 					h = img.getHeight()%height;			
 				}
-				
+				//---------------------------------- Multithread getSubimage?
 				images[i][j] = img.getSubimage(i*width,j*height, w, h);
 				
-				try {		
+				try {
+					//Saves to parent file of original image.
+					outputfile = new File(parentFile+ "\\"+fileInfo[0]+"_"+i+"-"+j+"."+fileInfo[fileInfo.length-1]);
 					//This will save to user's desktop. Normally it defaults to where the JAR is saved.
-				    outputfile = new File(System.getProperty("user.home") + "/Desktop\\"+fileInfo[0]+"_"+i+"-"+j+"."+fileInfo[fileInfo.length-1]);
+				    //outputfile = new File(System.getProperty("user.home") + "/Desktop\\"+fileInfo[0]+"_"+i+"-"+j+"."+fileInfo[fileInfo.length-1]);					
 				    ImageIO.write(images[i][j], fileInfo[fileInfo.length-1], outputfile);//Returns true if save succesful			    
 				} catch (IOException e) {
 					infoBox("Save failed!", "Error");
 				}
+				//----------------------------------
 			}
 		}
 		try {			
 			infoBox("Pictures saved to: "+outputfile.getCanonicalPath(), "Saved");
 		} catch (IOException e) {
-			infoBox("Pictures saved", "Saved");
+			infoBox("Pictures may not have been saved", "Saved");
 		}
 	}
 	
-	private void infoBox(String infoMessage, String title)
-    {
+
+	/**
+	 * Uses a filechooser for the user to select an image.
+	 */
+	private void getImage(){
+		JFileChooser fc = new JFileChooser();
+		if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+			try {
+			    img = ImageIO.read(fc.getSelectedFile());
+			} catch (IOException e) {
+				System.exit(1);
+			}
+		}
+		parentFile = fc.getSelectedFile().getParent();
+		fileInfo = fc.getName(fc.getSelectedFile()).split("\\.");
+	}
+	
+	/**
+	 * A pop up infobox
+	 * @param infoMessage
+	 * @param title
+	 */
+	private void infoBox(String infoMessage, String title){
         JOptionPane.showMessageDialog(null, infoMessage, title, JOptionPane.INFORMATION_MESSAGE);
     }
 }
